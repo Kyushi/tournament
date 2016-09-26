db58b7c33c25221ba014c1d45a6a0263f59f1e3c
Franziskus Nakajima <franziskus.nakajima@gmail.com>
2016-09-26 17:25:35 +0200

    improve tournament.sql (include db creation), comments

--- tournament.sql
+++ tournament.sql
@@ -6,16 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Create brand new tournament database (drop previous if exists)
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
+-- Connect to tournament db
 \c tournament;
 
-
+-- Create table players, only serial (ID) and name necessary
 CREATE TABLE players (
   pid serial PRIMARY KEY,
   name text
 );
 
+-- Create table matches with serial (ID), winner, loser as id referenced from
+-- players table
 CREATE TABLE matches (
   mid serial PRIMARY KEY,
   winner integer references players (pid),
